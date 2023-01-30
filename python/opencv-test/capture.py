3216cf28e9cd809485930f279bd5099812b08496
Hiroyuki Komatsu <kom@narihara-lab.jp>
2023-01-30 05:47:29 +0000

    gstreamer interface was tested on linux

--- python/opencv-test/capture.py
+++ python/opencv-test/capture.py
@@ -3,17 +3,26 @@
 import sys
 import cv2
 
-source = 'file'
+src = ''
+dst = 'record'
 
 def main():
     i = 0;
-    if source == 'file':
-        capture = cv2.VideoCapture("./camera_h264.mp4")
+    if src == 'file':
+        capture = cv2.VideoCapture("./camera.h265")
     else:
         capture = cv2.VideoCapture(0) # specify cam # whwn input is a camera
         ##
         ## set camera parameter using capture.set(...)
         ##
+    print('AAAAAAAAAAAAA')
+    if dst == 'record':
+        #cc4 = cv2.VideoWriter_fourcc(*'m','p','4','v')
+        #output = cv2.VideoWriter('output.mp4', cc4, 30.0, (640, 480))
+#        gst_pipe = 'appsrc ! videoconvert ! filesink location=test'
+        gst_pipe = 'appsrc ! videoconvert ! x264enc ! mp4mux ! filesink location=test'
+        output = cv2.VideoWriter(gst_pipe, cv2.CAP_GSTREAMER, 30, (640, 480))
+    print('BBBBBBBBBBBB')
 
     if capture.isOpened() is False:
         return 1
@@ -31,12 +40,15 @@ def main():
             ##
 
             # show a frame
-            cv2.imshow('frame',frame)
-            if source == 'file':
+            if src == 'file':
                 # wait for 1/FR when souece is a file
                 k = cv2.waitKey(100)  # unit is ms
                 if k == ord('q'):
                     break
+            if dst == 'record':
+                output.write(frame)
+            else:
+                cv2.imshoe(frame)
             i += 1
             if i > 100:
                 break
@@ -45,6 +57,8 @@ def main():
             # Ctrl+C pressed
             break
 
+    if dst == 'record':
+        output.release()
     capture.release()
     cv2.destroyAllWindows()
 
